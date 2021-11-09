5d44e1f4a02488b2107862ffa369cb0f42fb6999
Palme412 <jakepalmer412@gmail.com>
2021-11-09 13:13:39 -0600

    restart with base level layout

---
+++ restart.js
@@ -0,0 +1,57 @@
+var canvas = document.querySelector("#game");
+var ctx = canvas.getContext("2d");
+
+ctx.moveTo(90, 540);
+ctx.lineTo(160 , 540);
+// ctx ( x , y )
+ctx.moveTo(180, 480);
+ctx.lineTo(250, 480);
+
+ctx.moveTo(40, 440);
+ctx.lineTo(110, 440);
+
+ctx.moveTo(170, 370);
+ctx.lineTo(450, 370);
+
+ctx.moveTo(220, 330);
+ctx.lineTo(290, 330);
+
+ctx.moveTo(420, 310);
+ctx.lineTo(490, 310);
+
+ctx.moveTo(380, 250);
+ctx.lineTo(310, 250);
+
+ctx.moveTo(270, 210);
+ctx.lineTo(200, 210);
+
+ctx.moveTo(160, 170);
+ctx.lineTo(90, 170);
+
+ctx.moveTo(200, 130);
+ctx.lineTo(700, 130);
+
+ctx.moveTo(720, 90);
+ctx.lineTo(740, 90);
+
+ctx.moveTo(780, 50);
+ctx.lineTo(820, 50);
+
+ctx.moveTo(710, 160);
+ctx.lineTo(780, 160);
+
+ctx.moveTo(850, 270);
+ctx.lineTo(940, 270);
+
+ctx.moveTo(700, 320);
+ctx.lineTo(770, 320);
+
+ctx.moveTo(620, 430);
+ctx.lineTo(690, 430);
+
+ctx.moveTo(730, 500);
+ctx.lineTo(800, 500);
+
+ctx.moveTo(820, 550);
+ctx.lineTo(890, 550);
+ctx.stroke();
