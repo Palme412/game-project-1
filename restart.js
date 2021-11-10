3a9cd31b81e885b8d015a646641e8a5227f1b8c4
Palme412 <jakepalmer412@gmail.com>
2021-11-10 15:08:21 -0600

    changed level to rects

--- restart.js
+++ restart.js
@@ -2,24 +2,26 @@ var canvas = document.querySelector("#game");
 var ctx = canvas.getContext("2d");
 let img = new Image();
 img.src = "CharacterFull.png";
-img.onload = function () {
-    window.requestAnimationFrame(gameLoop)
-};
+// img.onload = function () {
+    // window.requestAnimationFrame(gameLoop)
+// };
 // ======== VARIABLES ========//
-const scale = 2;
-const width = 16;
-const height = 18;
-const scaledWidth = scale * width;
+width_canvas = document.querySelector("width");
+height_canvas = document.querySelector("height");
+const scale = 2; //player
+const width = 16; //player
+const height = 18; //player
+const scaledWidth = scale * width; 
 const scaledHeight = scale * height;
 const cycleLoop = [0, 1, 0, 2];
 let currentLoopIndex = 0;
 let frameCount = 0;
 const frameLimit = 12;
-const facingDown = 2;
+const facingDown = 2; //player
 const facingUp = 0;
 const facingLeft = 3;
-const facingRight = 1;
-let currentDirection = facingDown;
+const facingRight = 1; //player
+let currentDirection = facingDown; //player
 let keyPresses = {};
 const movement_Speed = 2;
 let positionX = 0;
@@ -39,7 +41,6 @@ function keyUpListener(event) {
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawPlatforms();
 
     let hasMoved = false;
 
@@ -93,65 +94,40 @@ function drawFrame(frameX, frameY, canvasX, canvasY) {
 }
 
 // ======= CANVAS OBJECTS======//
+// ctx.beginPath();
+// ========== Y Axis 600 ==== X Axis 1000 ======= X left-right               // Y top-bottom
+    ctx.fillStyle = "brown";
+    ctx.fillRect(90, 510, 70, 5);
+    ctx.fillRect(190, 460, 70, 5);
+    ctx.fillRect(290, 410, 70, 5);
+    ctx.fillRect(10, 340, 240, 5);
+    ctx.fillRect(20, 270, 30, 5);
+    ctx.fillRect(80, 210, 70, 5);
+    ctx.fillRect(30, 150, 20, 5);
+    ctx.fillRect(90, 100, 20, 5);
+    ctx.fillRect(30, 50, 20, 5);
+    ctx.fillRect(290, 270, 70, 5);
+    ctx.fillRect(370, 200, 70, 5);
+    ctx.fillRect(270, 150, 70, 5);
+    ctx.fillRect(370, 90, 350, 5);
+    ctx.fillRect(790, 180, 70, 5);
+    ctx.fillRect(1000, 80, -50, 5);
+    ctx.fillRect(880, 120, 10, 5);
+    ctx.fillRect(1000, 300, -400, 5);
+    ctx.fillRect(570, 410, 70, 5);
+    ctx.fillRect(700, 480, 70, 5);
+    ctx.fillRect(810, 420, 20, 5);
+    ctx.fillRect(1000, 370, -100, 5);
+    ctx.fillRect(600, 550, 70, 5);
+    ctx.fillRect(400, 600, 5, -100);
+    ctx.fillRect(500, 600, 5, -100);
+    ctx.fillRect(400, 570, 20, 5);
+    ctx.fillRect(500, 540, -20, 5);
+    ctx.fillRect(400, 500, 40, 5);
+
+
+ctx.clearRect(0, 0, width, height);
 ctx.beginPath();
-
-function drawPlatforms() {
-    ctx.moveTo(90, 540);
-    ctx.lineTo(160, 540);
-
-    ctx.moveTo(180, 480);
-    ctx.lineTo(250, 480);
-
-    ctx.moveTo(40, 440);
-    ctx.lineTo(110, 440);
-
-    ctx.moveTo(170, 370);
-    ctx.lineTo(450, 370);
-
-    ctx.moveTo(220, 330);
-    ctx.lineTo(290, 330);
-
-    ctx.moveTo(420, 310);
-    ctx.lineTo(490, 310);
-
-    ctx.moveTo(380, 250);
-    ctx.lineTo(310, 250);
-
-    ctx.moveTo(270, 210);
-    ctx.lineTo(200, 210);
-
-    ctx.moveTo(160, 170);
-    ctx.lineTo(90, 170);
-
-    ctx.moveTo(200, 130);
-    ctx.lineTo(700, 130);
-
-    ctx.moveTo(720, 90);
-    ctx.lineTo(740, 90);
-
-    ctx.moveTo(780, 50);
-    ctx.lineTo(820, 50);
-
-    ctx.moveTo(710, 160);
-    ctx.lineTo(780, 160);
-
-    ctx.moveTo(850, 270);
-    ctx.lineTo(940, 270);
-
-    ctx.moveTo(700, 320);
-    ctx.lineTo(770, 320);
-
-    ctx.moveTo(620, 430);
-    ctx.lineTo(690, 430);
-
-    ctx.moveTo(730, 500);
-    ctx.lineTo(800, 500);
-
-    ctx.moveTo(820, 550);
-    ctx.lineTo(890, 550);
-    ctx.stroke();
-    }
-
 
 
 //======= COLLISION LOGIC======= //
