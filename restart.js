928424c4127e25478b1d863a6a0be26f0d627932
Palme412 <jakepalmer412@gmail.com>
2021-11-10 17:56:57 -0600

    level layout with box pushes

--- restart.js
+++ restart.js
@@ -1,134 +1,212 @@
-var canvas = document.querySelector("#game");
-var ctx = canvas.getContext("2d");
-let img = new Image();
-img.src = "CharacterFull.png";
-// img.onload = function () {
-    // window.requestAnimationFrame(gameLoop)
-// };
-// ======== VARIABLES ========//
-width_canvas = document.querySelector("width");
-height_canvas = document.querySelector("height");
-const scale = 2; //player
-const width = 16; //player
-const height = 18; //player
-const scaledWidth = scale * width; 
-const scaledHeight = scale * height;
-const cycleLoop = [0, 1, 0, 2];
-let currentLoopIndex = 0;
-let frameCount = 0;
-const frameLimit = 12;
-const facingDown = 2; //player
-const facingUp = 0;
-const facingLeft = 3;
-const facingRight = 1; //player
-let currentDirection = facingDown; //player
-let keyPresses = {};
-const movement_Speed = 2;
-let positionX = 0;
-let positionY = 0;
+var canvas = document.querySelector("#game"),
+    ctx = canvas.getContext("2d"),
+    width = 1000,
+    height = 600,
+    playerwidth = 30,
+    playerheight = 30,
+    player = {
+        x: width / 2 - (playerwidth / 2),
+        y: height - 80,
+        width: playerwidth,
+        height: playerheight,
+        color: "red",
+        speed: 3,
+    },
+    boxes = [];
 
-// ========== GAME PROCESSES/CHARACTER MOVEMENT ========//
-window.addEventListener('keydown', keyDownListener);
-function keyDownListener(event) {
-    keyPresses[event.key] = true;
+boxes.push({
+    x: 0,
+    y: 0, //border
+    width: 5,
+    height: height
+});
+boxes.push({
+    x: 0,
+    y: height - 5,
+    width: width, //border
+    height: 50
+});
+
+boxes.push({
+    x: width - 5,
+    y: 0,
+    width: 5,   //border
+    height: height
+});
+boxes.push({
+    x: 0,
+    y: 0,
+    width: width,
+    height: 5
+})
+
+boxes.push({
+    x: 90,
+    y: 510,      //platforms all the way down
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 190,
+    y: 460,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 290,
+    y: 410,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 10,
+    y: 340,
+    width: 240,
+    height: 5
+});
+boxes.push({
+    x: 20,
+    y: 270,
+    width: 30,
+    height: 5
+});
+boxes.push({
+    x: 80,
+    y: 210,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 300,
+    y: 150,
+    width: 20,
+    height: 5
+});
+boxes.push({
+    x: 90,
+    y: 100,
+    width: 20,
+    height: 5
+});
+boxes.push({
+    x: 30,
+    y: 50,
+    width: 20,
+    height: 5
+});
+boxes.push({
+    x: 290,
+    y: 270,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 370,
+    y: 200,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 270,
+    y: 150,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 370,
+    y: 90,
+    width: 350,
+    height: 5
+});
+boxes.push({
+    x: 790,
+    y: 180,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 995,
+    y: 80,
+    width: -50,
+    height: 5
+});
+boxes.push({
+    x: 880,
+    y: 120,
+    width: 10,
+    height: 5
+});
+boxes.push({
+    x: 995,
+    y: 300,
+    width: -400,
+    height: 5
+});
+boxes.push({
+    x: 570,
+    y: 410,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 700,
+    y: 480,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 810,
+    y: 420,
+    width: 20,
+    height: 5
+});
+boxes.push({
+    x: 995,
+    y: 370,
+    width: -100,
+    height: 5
+});
+boxes.push({
+    x: 600,
+    y: 550,
+    width: 70,
+    height: 5
+});
+boxes.push({
+    x: 400,
+    y: 595,
+    width: 5,
+    height: -95
+});
+boxes.push({
+    x: 500,
+    y: 595,
+    width: 5,
+    height: -100
+});
+boxes.push({
+    x: 405,
+    y: 570,
+    width: 20,
+    height: 5
+});
+boxes.push({
+    x: 500,
+    y: 540,
+    width: -20,
+    height: 5
+});
+boxes.push({
+    x: 405,
+    y: 500,
+    width: 40,
+    height: 5
+});
+
+canvas.width = width;
+canvas.height = height;
+
+for (var i = 0; i < boxes.length; i++) {
+    ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+    ctx.fill();
 }
-
-window.addEventListener('keyup', keyUpListener);
-function keyUpListener(event) {
-    keyPresses[event.key] = false;
-}
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
-    let hasMoved = false;
-
-    if (keyPresses.w) {
-        moveCharacter(0, -movement_Speed, facingUp);
-        hasMoved = true;
-    } else if (keyPresses.s) {
-        moveCharacter(0, movement_Speed, facingDown)
-        hasMoved = true;
-    }
-    if (keyPresses.a) {
-        moveCharacter(-movement_Speed, 0, facingLeft);
-        hasMoved = true;
-    } else if (keyPresses.d) {
-        moveCharacter(movement_Speed, 0, facingRight);
-        hasMoved = true;
-    }
-    if (hasMoved) {
-        frameCount++;
-        if (frameCount >= frameLimit) {
-            frameCount = 0;
-            currentLoopIndex++;
-            if (currentLoopIndex >= cycleLoop.length) {
-                currentLoopIndex = 0;
-            }
-        }
-    }
-
-    if (!hasMoved) {
-        currentLoopIndex = 0;
-    }
-
-    drawFrame(cycleLoop[currentLoopIndex],
-        currentDirection, positionX, positionY);
-
-    window.requestAnimationFrame(gameLoop);
-}
-
-function moveCharacter(deltaX, deltaY, direction) {
-    if (positionX + deltaX > 0 && positionX + scaledWidth + deltaX < canvas.width) {
-        positionX += deltaX;
-    }
-    if (positionY + deltaY > 0 && positionY + scaledHeight + deltaY < canvas.height) {
-        positionY += deltaY;
-    }
-    currentDirection = direction;
-}
-
-function drawFrame(frameX, frameY, canvasX, canvasY) {
-    ctx.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, scaledWidth, scaledHeight);
-}
-
-// ======= CANVAS OBJECTS======//
-// ctx.beginPath();
-// ========== Y Axis 600 ==== X Axis 1000 ======= X left-right               // Y top-bottom
-    ctx.fillStyle = "brown";
-    ctx.fillRect(90, 510, 70, 5);
-    ctx.fillRect(190, 460, 70, 5);
-    ctx.fillRect(290, 410, 70, 5);
-    ctx.fillRect(10, 340, 240, 5);
-    ctx.fillRect(20, 270, 30, 5);
-    ctx.fillRect(80, 210, 70, 5);
-    ctx.fillRect(30, 150, 20, 5);
-    ctx.fillRect(90, 100, 20, 5);
-    ctx.fillRect(30, 50, 20, 5);
-    ctx.fillRect(290, 270, 70, 5);
-    ctx.fillRect(370, 200, 70, 5);
-    ctx.fillRect(270, 150, 70, 5);
-    ctx.fillRect(370, 90, 350, 5);
-    ctx.fillRect(790, 180, 70, 5);
-    ctx.fillRect(1000, 80, -50, 5);
-    ctx.fillRect(880, 120, 10, 5);
-    ctx.fillRect(1000, 300, -400, 5);
-    ctx.fillRect(570, 410, 70, 5);
-    ctx.fillRect(700, 480, 70, 5);
-    ctx.fillRect(810, 420, 20, 5);
-    ctx.fillRect(1000, 370, -100, 5);
-    ctx.fillRect(600, 550, 70, 5);
-    ctx.fillRect(400, 600, 5, -100);
-    ctx.fillRect(500, 600, 5, -100);
-    ctx.fillRect(400, 570, 20, 5);
-    ctx.fillRect(500, 540, -20, 5);
-    ctx.fillRect(400, 500, 40, 5);
-
-
-ctx.clearRect(0, 0, width, height);
-ctx.beginPath();
-
-
-//======= COLLISION LOGIC======= //
-
